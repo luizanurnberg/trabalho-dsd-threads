4180e4e0e5b4ac00006f844dd10068d7b8db2e74
luizanurnberg <nurnbergluiza@gmail.com>
2024-04-29 21:45:14 -0300

    feat(icons): add icons to grid matrix

--- trafficThreads/src/view/Simulation.java
+++ trafficThreads/src/view/Simulation.java
@@ -12,14 +12,16 @@ public class Simulation extends JFrame {
     private int numSimultaneousVehicles;
     private int rangeInsertion;
     private int[][] grid;
+    private Icon[][] icons;
 
-    public Simulation(int[][] grid, int exclusionType, int numVehicles, int numSimultaneousVehicles, int rangeInsertion) {
+    public Simulation(int[][] grid, Icon[][] icons, int exclusionType, int numVehicles, int numSimultaneousVehicles, int rangeInsertion) {
         super("Simulation");
         this.grid = grid;
         this.exclusionType = exclusionType;
         this.numVehicles = numVehicles;
         this.numSimultaneousVehicles = numSimultaneousVehicles;
         this.rangeInsertion = rangeInsertion;
+        this.icons = icons;
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(900, 900);
@@ -28,9 +30,10 @@ public class Simulation extends JFrame {
 
         jpPainel = new JPanel(new GridLayout(grid.length, grid[0].length));
         jpPainel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
-                JLabel label = new JLabel(String.valueOf(grid[i][j]));
+                JLabel label = new JLabel(icons[i][j]);
                 label.setHorizontalAlignment(SwingConstants.CENTER);
                 jpPainel.add(label);
             }
